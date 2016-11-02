f465f8f1763a20d5c9ecf7d35229d85d6f2cd680
Mike Revoir <mike.revoir@duke.edu>
2016-11-02 14:14:45 -0400

    add defensive logic to prevent exceptions

--- skin/src/main/java/org/researchstack/skin/ui/OnboardingActivity.java
+++ skin/src/main/java/org/researchstack/skin/ui/OnboardingActivity.java
@@ -141,7 +141,7 @@ public class OnboardingActivity extends PinCodeActivity implements View.OnClickL
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+        if(grantResults != null && grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
             LogExt.i(OnboardingActivity.class, "Permission: "+permissions[0]+ "was "+grantResults[0]);
             //resume tasks needing this permission
         }
